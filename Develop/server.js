1554a9ac8f2d69872f6c38127d2a87b7055ea304
Abigail709 <101914320+Abigail709@users.noreply.github.com>
2022-06-07 17:55:31 -0500

    last

--- Develop/server.js
+++ Develop/server.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const path = require('path');
+const api = require('./public/assets/js/index');
 
 const PORT = process.env.PORT || 3001;
 
-
 const app = express();
-
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
+app.use(express.static('public'));
 
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
@@ -21,7 +20,6 @@ app.get('/', (req, res) =>
 app.get('/feedback', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
 );
-
 
 
 app.listen(PORT, () =>
